d90e7f1044129aad2e815090451d6fcc799890c1
Christopher Allène <christopher.allene@mobsuccess.com>
2023-11-16 22:05:08 +0000

    Add footer

--- opengluck-server/app/src/pages/_document.tsx
+++ opengluck-server/app/src/pages/_document.tsx
@@ -1,4 +1,4 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
@@ -7,7 +7,24 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <footer>
+          This is{" "}
+          <a href="https://opengluck.com" target="_blank">
+            OpenGlück Server
+          </a>{" "}
+          - v1.0 -{" "}
+          <a
+            href="http://github.com/open-gluck/opengluck-server/wiki"
+            target="_blank"
+          >
+            Help
+          </a>{" "}
+          -{" "}
+          <a href="http://github.com/open-gluck/" target="_blank">
+            GitHub
+          </a>
+        </footer>
       </body>
     </Html>
-  )
+  );
 }
