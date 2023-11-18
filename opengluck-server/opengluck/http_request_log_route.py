b6421bb0fa5c356d14fb89b6b33bc7e468f00695
Christopher Allène <66381046+callms@users.noreply.github.com>
2023-11-18 09:54:16 +0100

    Fix log app requests only for admins (#16)

--- opengluck-server/opengluck/http_request_log_route.py
+++ opengluck-server/opengluck/http_request_log_route.py
@@ -3,12 +3,12 @@ import json
 from flask import Response
 
 from .http_request_log import get_last_http_requests
-from .login import assert_current_request_logged_in
+from .login import assert_current_request_is_logged_in_as_admin
 from .server import app
 
 
 @app.route("/opengluck/last-requests")
 def _last_requests():
-    assert_current_request_logged_in()
+    assert_current_request_is_logged_in_as_admin()
     last_http_requests = get_last_http_requests()
     return Response(json.dumps(last_http_requests))
